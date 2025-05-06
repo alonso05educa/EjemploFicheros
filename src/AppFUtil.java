7706661b3d15ae66af62ac8995b4ed8f9496639e
alonso10hub <gutierrezalonso2005@gmail.com>
2025-05-06 12:57:25 +0200

    Añadir FUtil.java y AppFUtil.java para probar que existen los archivos

---
+++ src/AppFUtil.java
@@ -0,0 +1,8 @@
+
+
+public class AppFUtil {
+    public static void main(String[] args) {
+        System.out.println("¿Existe test.txt? " + FUtil.existe("test.txt"));
+        System.out.println("¿Existe archivo_inexistente.txt? " + FUtil.existe("archivo_inexistente.txt"));
+    }
+}
---
+++ src/FUtil.java
@@ -0,0 +1,11 @@
+		
+import java.io.File;
+		public class FUtil {
+		public static boolean existe(String filename) {
+		File file = new File(filename);
+		if (file.exists()) {
+		return true;
+		}
+		return false;
+		}
+}
